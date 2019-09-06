72ab7025a2a790c78f71cefce1361dd9cf8ba711
KevSeenan <kev.seenan@gmail.com>
2019-09-06 20:00:09 +0100

    Adds solar system facts and added a div to html for PlanetsView.vue

--- src/components/SolarSystemFacts.vue
+++ src/components/SolarSystemFacts.vue
@@ -1,9 +1,24 @@
 <template lang="html">
-
+  <div id="solarSystemFacts">
+    <h1>Facts about our solar system</h1>
+    <h6>What does solar system mean?</h6>
+    <p>"Solar" means of the sun. "System" means a collection of objects that interact to form a whole.</p>
+    <p>Our solar system is located in an outer spiral rim of the Milky Way galaxy.</p>
+    <h6>Profile</h6>
+    <ul>
+      <li>Age: 4.6 Billion Years</li>
+      <li>Number of Planets: 8</li>
+      <li>Number of Moons: 181</li>
+      <li>Number of Asteroids: 552,894</li>
+      <li>Number of Comets: 3, 083</li>
+      <li>Diameter: 18.75 trillion km (11.65 trillion miles)</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
+  name: "solar-system-facts"
 }
 </script>
 
