5edd82f7a8c763a089dbd4cb447f71ecd05de451
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-06 18:02:36 +0100

    adds router link to Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,22 +1,35 @@
- <template lang="html">
-   <div id="header">
-     <h3>Home</h3>
-     <h3>Quiz</h3>
-     <PlanetsList :planets="planets" />
-   </div>
- </template>
+<template lang="html">
+  <div id="header">
+    <h1>CCG</h1>
+    <h6>CodeClan Glasgow</h6>
+    <div id="nav">
+      <router-link :to="{ name: 'planets-view'}"></router-link>
+      <h3>Quiz</h3>
+    </div>
+    <PlanetsList :planets="planets" />
+  </div>
+</template>
 
- <script>
- import PlanetsList from '@/components/PlanetsList';
+<script>
+import PlanetsList from '@/components/PlanetsList';
 
- export default {
-   name: "header",
-   props: ["planets"],
-   components: {
-     PlanetsList
-   }
- }
- </script>
+export default {
+  name: "header",
+  props: ["planets"],
+  components: {
+    PlanetsList
+  }
+}
+</script>
 
- <style lang="css" scoped>
- </style>
+<style lang="css" scoped>
+#header{
+  display: flex;
+  flex-direction: column;
+}
+
+#nav{
+  display: flex;
+  flex-direction: column;
+}
+</style>
---
+++ src/router.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+
+import PlanetsView from './views/PlanetsView';
+
+Vue.use(Router);
+
+const router = new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'planets-view',
+      component: PlanetsView
+    }
+  ];
+});
+
+export default router;
