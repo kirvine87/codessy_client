db02ec3f1782d732edf96a9be02913fe64e02ae6
Kyle <kirvine87@gmail.com>
2019-09-06 20:13:26 +0100

    adds planet list and click

--- src/components/PlanetsList.vue
+++ src/components/PlanetsList.vue
@@ -1,9 +1,20 @@
 <template lang="html">
-
+  <div class="planet">
+    <h3 @click="handlePlanetClick(planet)" :key="index" v-for="(planet, index) in planets">{{planet.name}}</h3>
+  </div>
 </template>
 
 <script>
+import {eventBus} from '@/main.js';
+
 export default {
+  name: 'planets-list',
+  props: ['planets'],
+  methods: {
+    handlePlanetClick(planet) {
+      eventBus.$emit('planet-selected', planet)
+    }
+  }
 }
 </script>
 
