fa6b7559e07ff1bcb890c7fa336df20307a15b84
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-06 17:29:35 +0100

    sets up Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,20 @@
  <template lang="html">
-
+   <div id="header">
+     <h3>Home</h3>
+     <h3>Quiz</h3>
+     <PlanetsList :planets="planets" />
+   </div>
  </template>
 
  <script>
+ import PlanetsList from '@/components/PlanetsList';
+
  export default {
+   name: "header",
+   props: ["planets"],
+   components: {
+     PlanetsList
+   }
  }
  </script>
 
