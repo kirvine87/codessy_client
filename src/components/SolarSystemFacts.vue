95edb0c68028c247f8ccd7cca99da36ba0bc3be8
KevSeenan <kev.seenan@gmail.com>
2019-09-07 15:52:27 +0100

    Adds inner solar system info to SolarSystemFacts

--- src/components/SolarSystemFacts.vue
+++ src/components/SolarSystemFacts.vue
@@ -1,10 +1,10 @@
 <template lang="html">
   <div id="solarSystemFacts">
     <h1>Facts about our solar system</h1>
-    <h6>What does solar system mean?</h6>
+    <h3>What does solar system mean?</h3>
     <p>"Solar" means of the sun. "System" means a collection of objects that interact to form a whole.</p>
     <p>Our solar system is located in an outer spiral rim of the Milky Way galaxy.</p>
-    <h6>Profile</h6>
+    <h3>Profile</h3>
     <ul>
       <li>Age: 4.6 Billion Years</li>
       <li>Number of Planets: 8</li>
@@ -13,14 +13,24 @@
       <li>Number of Comets: 3, 083</li>
       <li>Diameter: 18.75 trillion km (11.65 trillion miles)</li>
     </ul>
-  </div>
-</template>
+    <h3>The Planets</h3>
+    <p>The planets in our solar system (in order of distance from the sun) are Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, and Neptune.</p>
+    <p>These are located in both the inner and outer solar system.</p>
+    <h3>The Inner Solar System</h3>
+    <p>The inner solar system is the region made up of Mercury, Venus, Earth and Mars as well as the asteroid belt.
+      These planets are known as terrestrial planets and are composed mainly of silicates and metals. Terrestrial planets are relatively close to the sun and the radius of this entire region is less than the distance between the orbits of Jupiter and Saturn.
+      The four planets within this region have dense, rocky compositions, few or no moons and no ring systems. Venus, Earth and Mars have atmospheres that are able to generate weather.
+      They also have impact craters and tectonic surface features, such as rift valleys and volcanoes.
+      Mercury and Venus are also known as inferior planets because they are closer to the sun than Earth. </p>
+    
+    </div>
+  </template>
 
-<script>
-export default {
-  name: "solar-system-facts"
-}
-</script>
+  <script>
+  export default {
+    name: "solar-system-facts"
+  }
+  </script>
 
-<style lang="css" scoped>
-</style>
+  <style lang="css" scoped>
+  </style>
