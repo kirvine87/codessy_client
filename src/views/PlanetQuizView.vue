2b0b42eb9ceb33cb563c0ab2924c4c0d090cb1f7
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-09 12:16:02 +0100

    sets up main quiz view with fetch for quiz information and preparation for PlanetSelect component - commented out

--- src/views/PlanetQuizView.vue
+++ src/views/PlanetQuizView.vue
@@ -1,9 +1,33 @@
 <template lang="html">
-
+  <div id="quiz-view">
+    <h1>Test your Solar System Knowledge</h1>
+    <!-- <PlanetSelect :quizes="quizes" /> -->
+  </div>
 </template>
 
 <script>
+// import PlanetSelect from '@/components/PlanetSelect';
+
 export default {
+  name: "planet-quiz-view",
+  data(){
+    return{
+      quizes: []
+    }
+  },
+  mounted(){
+    this.fetchQuizes();
+  },
+  methods: {
+    fetchQuizes() {
+      fetch("http://localhost:3000/api/quizes")
+      .then(res => res.json())
+      .then(quizes => this.quizes = quizes)
+    }
+  },
+  components: {
+    // PlanetSelect
+  }
 }
 </script>
 
