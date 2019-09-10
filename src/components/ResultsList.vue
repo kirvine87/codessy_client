435d91b648597875191f08f3cd7eac0288ed9fc6
KevSeenan <kev.seenan@gmail.com>
2019-09-10 13:57:51 +0100

    Adds button to take a quiz if it has not been done. Working on functionality of button

--- src/components/ResultsList.vue
+++ src/components/ResultsList.vue
@@ -1,7 +1,10 @@
 <template lang="html">
   <div id="results">
-    <h3 :key="index" v-for="(result, index) in results">{{result.name}}: </h3>
-    <h3 v-if="result" :result="result">{{result.result}}</h3>
+    <div :key="index" v-for="(result, index) in results">
+      <h3>{{result.name}}:</h3>
+      <h3 v-if="result.result">{{result.result}}</h3>
+      <button v-if="!result.result" v-on:click="handleTakeQuizClick(quiz)">Take Quiz</button>
+    </div>
   </div>
 </template>
 
@@ -11,7 +14,12 @@ import {eventBus} from '@/main.js';
 
 export default {
   name: 'results-list',
-  props: ['results']
+  props: ['results'],
+  methods: {
+    handleTakeQuizClick(quiz) {
+      eventBus.$emit('quiz-select', quiz)
+    }
+  }
 }
 </script>
 
