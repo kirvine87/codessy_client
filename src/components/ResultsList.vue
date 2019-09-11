192d7c255f03b8b1f7e643b741f73a92fb261aef
Gillian Shanks <GillianShanks88@gmail.com>
2019-09-11 11:48:30 +0100

    adds astronaut on results page

--- src/components/ResultsList.vue
+++ src/components/ResultsList.vue
@@ -1,17 +1,33 @@
 <template lang="html">
+  <div id="results-image">
+
   <div id="results">
-    <h2 v-if="totalAverage >= 0.00">Your Average So Far: {{totalAverage}}%</h2>
+    <div v-if="totalAverage >= 0.00">
+      <h2 id="avg">Your Average Score So Far: </h2>
+      <p>{{totalAverage}}%</p>
+    </div>
+
     <div :key="index" v-for="(result, index) in results">
       <h3>{{result.name}}:</h3>
-      <h3 v-if="result.result">You got {{result.result}}%!</h3>
+      <div class="has-score">
+        <h3 v-if="result.result">You got {{result.result}}%!</h3>
+        <div class="re-take-btn">
+          <router-link :to="{ name: 'planets-quiz-view', params: {planetName:result.name} }">
+            <button v-if="result.result">Re-Take Quiz</button>
+          </router-link>
+        </div>
+
+      </div>
+
       <router-link :to="{ name: 'planets-quiz-view', params: {planetName:result.name} }">
         <button v-if="!result.result">Take Quiz</button>
       </router-link>
-      <router-link :to="{ name: 'planets-quiz-view', params: {planetName:result.name} }">
-        <button v-if="result.result">Re-Take Quiz</button>
-      </router-link>
+
     </div>
   </div>
+
+  <img src="https://media.discordapp.net/attachments/471000631700619284/621124237943373824/astronaut-rectangle-flip.png" alt="female astronaut giving thumbs up">
+</div>
 </template>
 
 <script>
@@ -49,15 +65,54 @@ export default {
 
 <style lang="css" scoped>
 
+#results-image{
+  display: flex;
+  flex-direction: row;
+  flex:1;
+  justify-content: space-between;
+}
+
+#results{
+  height: 78vh;
+  width: 30vw;
+  overflow: scroll;
+  background-color: rgba(255, 255, 255, 0.4);
+  border: 2px dashed white;
+  padding-left: 1em;
+}
+
+img{
+  height: 80vh;
+  object-fit:cover;
+}
+
 button {
-  margin-top: 1em;
+  margin: 0.5em;
   background-color: rgba(126, 70, 154, 0.6);
   color: #FFFFFF;
   font-size: 1.2em;
+
 }
 
 button:hover {
   cursor: pointer;
 }
 
+h3{
+  margin:1em 0 0 0;
+  padding:0;
+}
+
+.has-score{
+  display: flex;
+  flex-direction: row;
+}
+
+p{
+  font-size: 1.5em;
+  text-align: center;
+}
+#avg{
+  text-align: center;
+}
 </style>
